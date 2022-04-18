cfaa1b26fa5b0744f8a831cc38804a8ae9404247
Doug Strain <dstrain@google.com>
2022-04-18 13:15:18 -0700

    Get rid of optimize directory and readout_scan

--- unitary/__init__.py
+++ unitary/__init__.py
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from unitary import optimize
-
 from unitary.serialization_utils import (
     exists,
     save,
