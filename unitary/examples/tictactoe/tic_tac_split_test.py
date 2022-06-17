7878cc29e1e200db0db3b8ebdfa63a52d39d8920
Evert van Nieuwenburg <evert.v.nieuwenburg@gmail.com>
2022-06-17 14:03:43 +0200

    Split now uses either custom swap gate or sqrt-ISWAP depending on ruleset

--- unitary/examples/tictactoe/tic_tac_split_test.py
+++ unitary/examples/tictactoe/tic_tac_split_test.py
@@ -19,6 +19,10 @@ import cirq
 import unitary.alpha as alpha
 import unitary.examples.tictactoe as tictactoe
 
+_E = tictactoe.TicTacSquare.EMPTY
+_O = tictactoe.TicTacSquare.O
+_X = tictactoe.TicTacSquare.X
+
 
 @pytest.mark.parametrize(
     "mark, swap_state1, swap_state2",
@@ -78,12 +82,17 @@ b (d=3): ───×X───
     )
 
 
-@pytest.mark.parametrize("mark", [tictactoe.TicTacSquare.X, tictactoe.TicTacSquare.O])
-def test_tic_tac_split(mark: tictactoe.TicTacSquare):
+@pytest.mark.parametrize("mark, ruleset",
+    [(tictactoe.TicTacSquare.X, tictactoe.TicTacRules.QUANTUM_V2),
+    (tictactoe.TicTacSquare.X, tictactoe.TicTacRules.QUANTUM_V3),
+    (tictactoe.TicTacSquare.O, tictactoe.TicTacRules.QUANTUM_V2),
+    (tictactoe.TicTacSquare.O, tictactoe.TicTacRules.QUANTUM_V3)])
+def test_tic_tac_split(mark: tictactoe.TicTacSquare,
+                       ruleset: tictactoe.TicTacRules):
     a = alpha.QuantumObject("a", tictactoe.TicTacSquare.EMPTY)
     b = alpha.QuantumObject("b", tictactoe.TicTacSquare.EMPTY)
     board = alpha.QuantumWorld([a, b])
-    tictactoe.TicTacSplit(mark)(a, b)
+    tictactoe.TicTacSplit(mark, ruleset)(a, b)
     results = board.peek(count=1000)
     on_a = [mark, tictactoe.TicTacSquare.EMPTY]
     on_b = [tictactoe.TicTacSquare.EMPTY, mark]
@@ -91,38 +100,40 @@ def test_tic_tac_split(mark: tictactoe.TicTacSquare):
     assert any(r == on_b for r in results)
     assert all(r == on_a or r == on_b for r in results)
 
-
-def test_tic_tac_split_entangled():
+def test_tic_tac_split_entangled_v2():
     a = alpha.QuantumObject("a", tictactoe.TicTacSquare.EMPTY)
     b = alpha.QuantumObject("b", tictactoe.TicTacSquare.EMPTY)
     c = alpha.QuantumObject("c", tictactoe.TicTacSquare.EMPTY)
     board = alpha.QuantumWorld([a, b, c])
-    tictactoe.TicTacSplit(tictactoe.TicTacSquare.X)(a, b)
-    tictactoe.TicTacSplit(tictactoe.TicTacSquare.O)(b, c)
+    ruleset = tictactoe.TicTacRules.QUANTUM_V2
+    tictactoe.TicTacSplit(tictactoe.TicTacSquare.X, ruleset)(a, b)
+    tictactoe.TicTacSplit(tictactoe.TicTacSquare.O, ruleset)(b, c)
+    results = board.peek(count=1000)
+    on_ab = [ _X, _O, _E ]
+    on_ac = [ _X, _E, _O ]
+    on_b  = [ _E, _X, _E ]
+    assert any(r == on_ab for r in results)
+    assert any(r == on_ac for r in results)
+    assert any(r == on_b for r in results)
+    assert all(r == on_ab or r == on_ac or r == on_b for r in results)
+
+
+def test_tic_tac_split_entangled_v3():
+    a = alpha.QuantumObject("a", tictactoe.TicTacSquare.EMPTY)
+    b = alpha.QuantumObject("b", tictactoe.TicTacSquare.EMPTY)
+    c = alpha.QuantumObject("c", tictactoe.TicTacSquare.EMPTY)
+    board = alpha.QuantumWorld([a, b, c])
+    ruleset = tictactoe.TicTacRules.QUANTUM_V3
+    tictactoe.TicTacSplit(tictactoe.TicTacSquare.X, ruleset)(a, b)
+    tictactoe.TicTacSplit(tictactoe.TicTacSquare.O, ruleset)(b, c)
     results = board.peek(count=1000)
     # This sequence of moves, for just three squares, should produce:
     # EEE -> XEE + EXE -> XEO + EXE -> XEO + XOE + EXE + EEX
     # The checks below check whether each of these last four samples occurs
-    sample_1 = [
-        tictactoe.TicTacSquare.X,
-        tictactoe.TicTacSquare.O,
-        tictactoe.TicTacSquare.EMPTY,
-    ]
-    sample_2 = [
-        tictactoe.TicTacSquare.X,
-        tictactoe.TicTacSquare.EMPTY,
-        tictactoe.TicTacSquare.O,
-    ]
-    sample_3 = [
-        tictactoe.TicTacSquare.EMPTY,
-        tictactoe.TicTacSquare.X,
-        tictactoe.TicTacSquare.EMPTY,
-    ]
-    sample_4 = [
-        tictactoe.TicTacSquare.EMPTY,
-        tictactoe.TicTacSquare.EMPTY,
-        tictactoe.TicTacSquare.X,
-    ]
+    sample_1 = [ _X, _O, _E ]
+    sample_2 = [ _X, _E, _O ]
+    sample_3 = [ _E, _X, _E ]
+    sample_4 = [ _E, _E, _X ]
     assert any(r == sample_1 for r in results)
     assert any(r == sample_2 for r in results)
     assert any(r == sample_3 for r in results)
@@ -131,37 +142,22 @@ def test_tic_tac_split_entangled():
                r == sample_3 or r == sample_4 for r in results)
 
 
-def test_tic_tac_split_entangled_empty():
+def test_tic_tac_split_entangled_v3_empty():
     a = alpha.QuantumObject("a", tictactoe.TicTacSquare.EMPTY)
     b = alpha.QuantumObject("b", tictactoe.TicTacSquare.EMPTY)
     c = alpha.QuantumObject("c", tictactoe.TicTacSquare.EMPTY)
     board = alpha.QuantumWorld([a, b, c])
-    tictactoe.TicTacSplit(tictactoe.TicTacSquare.X)(a, b)
-    tictactoe.TicTacSplit(tictactoe.TicTacSquare.O)(c, b)
+    ruleset = tictactoe.TicTacRules.QUANTUM_V3
+    tictactoe.TicTacSplit(tictactoe.TicTacSquare.X, ruleset)(a, b)
+    tictactoe.TicTacSplit(tictactoe.TicTacSquare.O, ruleset)(c, b)
     results = board.peek(count=1000)
     # This sequence of moves, for just three squares, should produce:
     # EEE -> XEE + EXE -> XEO + EXO -> XEO + XOE + EXO + EOX
     # The checks below check whether each of these last four samples occurs
-    sample_1 = [
-        tictactoe.TicTacSquare.X,
-        tictactoe.TicTacSquare.O,
-        tictactoe.TicTacSquare.EMPTY,
-    ]
-    sample_2 = [
-        tictactoe.TicTacSquare.X,
-        tictactoe.TicTacSquare.EMPTY,
-        tictactoe.TicTacSquare.O,
-    ]
-    sample_3 = [
-        tictactoe.TicTacSquare.EMPTY,
-        tictactoe.TicTacSquare.X,
-        tictactoe.TicTacSquare.O,
-    ]
-    sample_4 = [
-        tictactoe.TicTacSquare.EMPTY,
-        tictactoe.TicTacSquare.O,
-        tictactoe.TicTacSquare.X,
-    ]
+    sample_1 = [ _X, _O, _E ]
+    sample_2 = [ _X, _E, _O ]
+    sample_3 = [ _E, _X, _O ]
+    sample_4 = [ _E, _O, _X ]
     assert any(r == sample_1 for r in results)
     assert any(r == sample_2 for r in results)
     assert any(r == sample_3 for r in results)
