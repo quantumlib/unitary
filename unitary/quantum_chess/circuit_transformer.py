380cc7ebf5854fe00056ff904f735e1cbe117bf4
Ricardo Olenewa <rolenewa@gmail.com>
2022-06-17 22:15:11 -0400

    Missed a reference.

--- unitary/quantum_chess/circuit_transformer.py
+++ unitary/quantum_chess/circuit_transformer.py
@@ -46,7 +46,7 @@ class ConnectivityHeuristicCircuitTransformer:
         super().__init__()
         self.device = device
         self.mapping = None
-        self.starting_qubit = self.find_start_qubit(device.qubits)
+        self.starting_qubit = self.find_start_qubit(device.metadata.qubit_set)
         self.qubit_list = device.metadata.qubit_set
 
     def qubits_within(
