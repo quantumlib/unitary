d2980e67c32e3120a046f33bb4f28707bf64e4a2
Conrad Holt <conrad.holt@protonmail.com>
2022-07-15 23:38:32 -0500

    Fix merge_slide_zero_one circuit
    Merge slide is not symmetrical with respect to swapping the source
    squares, so there needs to be a different circuit depending on which
    path is potentially blocked.
    
    Modified tests to make them sensitive to mistakenly reversing the
    sources in the circuit.

--- unitary/quantum_chess/quantum_board.py
+++ unitary/quantum_chess/quantum_board.py
@@ -1076,7 +1076,7 @@ class CirqBoard:
                 # (1, 0): No qubit in one arm, one qubit in the other. 0 ancilla needed.
                 if len(path_qubits) == 1:
                     self.circuit.append(
-                        qm.merge_slide_zero_one(squbit2, tqubit, squbit, path_qubits[0])
+                        qm.merge_slide_one_zero(squbit, tqubit, squbit2, path_qubits[0])
                     )
                 # (2+, 0): No qubit in one arm, multiple qubits in the other. 1 ancilla needed.
                 else:
