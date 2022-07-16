6beab5f821f3a05624494232a23bc7b75c90df21
Conrad Holt <conrad.holt@protonmail.com>
2022-07-16 01:39:18 -0500

    Fix (split|merge)_slide_one_multiple
    The split/merge slide circuits are not symmetric with respect to
    exchange of target/source qubits.

--- unitary/quantum_chess/quantum_board.py
+++ unitary/quantum_chess/quantum_board.py
@@ -1008,11 +1008,13 @@ class CirqBoard:
                 else:
                     path2 = self._create_path_ancilla(path_qubits2)
                     ancilla = self.new_ancilla()
+                    self.circuit.append(cirq.X(path_qubits[0]))
                     self.circuit.append(
-                        qm.split_slide_one_multiple(
+                        qm.split_slide(
                             squbit, tqubit, tqubit2, path_qubits[0], path2, ancilla
                         )
                     )
+                    self.circuit.append(cirq.X(path_qubits[0]))
                     self._clear_path_ancilla(path_qubits2, path2)
                 return 1
             # (2+, 1): one qubit in one arm, multiple qubits in the other. 2 ancillas needed.
@@ -1020,11 +1022,13 @@ class CirqBoard:
                 self.add_entangled(squbit, tqubit, tqubit2)
                 path1 = self._create_path_ancilla(path_qubits)
                 ancilla = self.new_ancilla()
+                self.circuit.append(cirq.X(path_qubits2[0]))
                 self.circuit.append(
-                    qm.split_slide_one_multiple(
-                        squbit, tqubit2, tqubit, path_qubits2[0], path1, ancilla
+                    qm.split_slide(
+                        squbit, tqubit, tqubit2, path1, path_qubits2[0], ancilla
                     )
                 )
+                self.circuit.append(cirq.X(path_qubits2[0]))
                 self._clear_path_ancilla(path_qubits, path1)
                 return 1
             # (2+, 2+): multiple qubits in both arms. 3 ancillas needed.
@@ -1115,11 +1119,13 @@ class CirqBoard:
                 else:
                     path2 = self._create_path_ancilla(path_qubits2)
                     ancilla = self.new_ancilla()
+                    self.circuit.append(cirq.X(path_qubits[0]))
                     self.circuit.append(
-                        qm.merge_slide_one_multiple(
+                        qm.merge_slide(
                             squbit, tqubit, squbit2, path_qubits[0], path2, ancilla
                         )
                     )
+                    self.circuit.append(cirq.X(path_qubits[0]))
                     self._clear_path_ancilla(path_qubits2, path2)
                 return 1
             # (2+, 1): one qubit in one arm, multiple qubits in the other. 2 ancillas needed.
@@ -1127,11 +1133,13 @@ class CirqBoard:
                 self.add_entangled(squbit, squbit2, tqubit)
                 path1 = self._create_path_ancilla(path_qubits)
                 ancilla = self.new_ancilla()
+                self.circuit.append(cirq.X(path_qubits2[0]))
                 self.circuit.append(
-                    qm.merge_slide_one_multiple(
-                        squbit2, tqubit, squbit, path_qubits2[0], path1, ancilla
+                    qm.merge_slide(
+                        squbit, tqubit, squbit2, path1, path_qubits2[0], ancilla
                     )
                 )
+                self.circuit.append(cirq.X(path_qubits2[0]))
                 self._clear_path_ancilla(path_qubits, path1)
                 return 1
             # (2+, 2+): multiple qubits in both arms. 3 ancillas needed.
