66a3d62d62a4d4284cfe71f57c8d4feaf4afa858
Smit <smitsanghavi22@gmail.com>
2022-10-13 23:47:28 -0700

    Add some memoization

--- unitary/alpha/qudit_to_qubit_transform.py
+++ unitary/alpha/qudit_to_qubit_transform.py
@@ -15,35 +15,82 @@
 import numpy as np
 
 
-def qudit_to_qubit_state(
-        dim_single_qudit: int, num_qudits: int, qudit_state_vector: np.ndarray, pad_value: np.complex_ = 0):
+def _nearest_power_of_two_ceiling(x: int) -> int:
+    exp = 0
+    while x:
+        x = x >> 1
+        exp += 1
+    return 2**exp
+
+
+def qudit_to_qubit_state_impl(
+    dim_single_qudit: int,
+    num_qudits: int,
+    qudit_state_vector: np.ndarray,
+    pad_value: np.complex_ = 0,
+):
     """This function converts a qudit-space quantum state vector to m-qubit-per-qudit column vector."""
-    num_qubits_per_qudit = np.ceil(np.log(dim_single_qudit)/np.log(2))
     # Reshape the state vector to a `num_qudits` rank tensor.
-    state_tensor = qudit_state_vector.reshape((dim_single_qudit,)*num_qudits)
+    state_tensor = qudit_state_vector.reshape((dim_single_qudit,) * num_qudits)
     # Number of extra elements needed in each dimension if represented using qubits.
-    padding_amount = int((2**num_qubits_per_qudit)-dim_single_qudit)
+    padding_amount = _nearest_power_of_two_ceiling(dim_single_qudit) - dim_single_qudit
     # Expand the number of elements in each dimension by the padding_amount. Fill
     # the new elements with the pad_value.
-    padded_state_tensor = np.pad(state_tensor,
-                                 (0, padding_amount),
-                                 constant_values=pad_value)
+    padded_state_tensor = np.pad(
+        state_tensor, (0, padding_amount), constant_values=pad_value
+    )
     # Return a flattened state vector view of the final tensor.
     return np.ravel(padded_state_tensor)
 
 
-def qudit_to_qubit_unitary(dim_single_qudit: int, num_qudits: int, qudit_unitary: np.ndarray):
+def qudit_to_qubit_from_map(
+    qudit_state_vector: np.ndarray, qubit_to_qudit_index_map: np.ndarray
+):
+    return [
+        qudit_state_vector[qubit_to_qudit_index_map[j] - 1]
+        if qubit_to_qudit_index_map[j]
+        else 0
+        for j in range(len(qubit_to_qudit_index_map))
+    ]
+
+
+def qudit_to_qubit_state(
+    dim_single_qudit: int,
+    num_qudits: int,
+    qudit_state_vectors: list[np.ndarray],
+    pad_value: np.complex_ = 0,
+) -> list[np.ndarray]:
+    qubit_to_qudit_index_map = qudit_to_qubit_state_impl(
+        dim_single_qudit,
+        num_qudits,
+        np.array([i + 1 for i in range(dim_single_qudit**num_qudits)]),
+        pad_value,
+    )
+    return [
+        qudit_to_qubit_from_map(state, qubit_to_qudit_index_map)
+        for state in qudit_state_vectors
+    ]
+
+
+def qudit_to_qubit_unitary(
+    dim_single_qudit: int, num_qudits: int, qudit_unitary: np.ndarray
+):
     """This function converts a qudit-space quantum state vector to m-qubit-per-qudit column vector."""
-    num_qubits_per_qudit = np.ceil(np.log(dim_single_qudit)/np.log(2))
+    num_qubits_per_qudit = np.ceil(np.log(dim_single_qudit) / np.log(2))
     num_qubits = num_qubits_per_qudit * num_qudits
     dim_qubit_space = int(2**num_qubits)
 
     # Treat the unitary as a num_qubits^2 system's state vector and represent it using qubits (pad with 0s).
-    padded_unitary = qudit_to_qubit_state(dim_single_qudit, num_qudits**2, np.ravel(qudit_unitary))
+    padded_unitary = qudit_to_qubit_state(
+        dim_single_qudit, num_qudits**2, np.ravel(qudit_unitary)
+    )
     # A qubit-based state vector with the extra padding bits having 1s and rest having 0s.
     pad_qubits_vector = qudit_to_qubit_state(
-        dim_single_qudit, num_qudits, np.zeros(dim_single_qudit**num_qudits), 1)
+        dim_single_qudit, num_qudits, np.zeros(dim_single_qudit**num_qudits), 1
+    )
     # Reshape the padded unitary to the final shape and add a diagonal matrix corresponding to the
     # pad_qubits_vector. This addition ensures that the invalid states with the "padding" bits map to identity,
     # preserving unitarity.
-    return padded_unitary.reshape(dim_qubit_space, dim_qubit_space) + np.diag(pad_qubits_vector)
+    return padded_unitary.reshape(dim_qubit_space, dim_qubit_space) + np.diag(
+        pad_qubits_vector
+    )
