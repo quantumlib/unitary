fc9ac6096205ab0f0d03949a55b2c5a694474ddd
madcpf <madcpf@gmail.com>
2023-06-02 21:56:50 -0700

    remove redundant )

--- unitary/examples/quantum_chinese_chess/test_utils.py
+++ unitary/examples/quantum_chinese_chess/test_utils.py
@@ -11,7 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-)
 a0 = cirq.NamedQubit('a0')
 a1 = cirq.NamedQubit('a1')
 a2 = cirq.NamedQubit('a2')
