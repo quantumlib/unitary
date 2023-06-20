3fa03f3d45528b6a66625478fba02ad33a295a23
Doug Strain <dstrain@google.com>
2023-06-20 15:50:21 -0700

    Add copy for encounter too.

--- unitary/examples/quantum_rpg/encounter_test.py
+++ unitary/examples/quantum_rpg/encounter_test.py
@@ -13,11 +13,13 @@
 # limitations under the License.
 
 import io
+import unitary.alpha as alpha
 import unitary.examples.quantum_rpg.battle as battle
 import unitary.examples.quantum_rpg.classes as classes
 import unitary.examples.quantum_rpg.game_state as game_state
 import unitary.examples.quantum_rpg.encounter as encounter
 import unitary.examples.quantum_rpg.npcs as npcs
+import unitary.examples.quantum_rpg.xp_utils as xp_utils
 
 
 def test_trigger():
@@ -57,3 +59,20 @@ Sample result HealthPoint.HURT
 Observer watcher measures Aaronson at qubit Aaronson_1
 """.strip()
     )
+
+
+def test_copy():
+    o = npcs.Observer("watcher")
+    xp = xp_utils.EncounterXp([[alpha.Flip()]], weights=[1])
+    e = encounter.Encounter([o], probability=0.75, description="Tester", xp=xp)
+    e2 = e.copy()
+    assert len(e.enemies) == len(e2.enemies)
+    assert e.enemies[0] is not e2.enemies[0]
+    qar = e.enemies[0]
+    qar2 = e2.enemies[0]
+    assert qar.circuit == qar2.circuit
+    assert qar.name == qar2.name
+    assert qar.level == qar2.level
+    assert e.probability == e2.probability
+    assert e.description == e2.description
+    assert e.xp == e2.xp
