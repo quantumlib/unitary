09a79d3638a309c0d507566a93470aab462bb0eb
Pengfei Chen <pengfeichen@google.com>
2023-10-02 14:50:13 -0700

    update format

--- unitary/examples/quantum_chinese_chess/piece.py
+++ unitary/examples/quantum_chinese_chess/piece.py
@@ -11,14 +11,12 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from unitary.alpha import (
-    QuantumObject
-)
+from unitary.alpha import QuantumObject
 from unitary.examples.quantum_chinese_chess.enums import (
     SquareState,
     Language,
     Color,
-    Type
+    Type,
 )
 
 
