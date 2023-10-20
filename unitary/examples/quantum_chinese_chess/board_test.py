5883cacf32583ec5dce8fbe5c556c22a42fb7d13
madcpf <madcpf@gmail.com>
2023-10-19 21:48:51 -0700

    update

---
+++ unitary/examples/quantum_chinese_chess/board_test.py
@@ -0,0 +1,189 @@
+# Copyright 2023 The Unitary Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+from unitary.examples.quantum_chinese_chess.enums import (
+    Language,
+    Color,
+    Type,
+    SquareState,
+)
+from unitary.examples.quantum_chinese_chess.board import *
+from unitary.examples.quantum_chinese_chess.piece import Piece
+from unitary.examples.quantum_chinese_chess.test_utils import (
+    locations_to_bitboard,
+    assert_samples_in,
+    assert_sample_distribution,
+    get_board_probability_distribution,
+    set_board,
+)
+
+
+# def test_init_with_default_fen():
+#     board = Board.from_fen()
+#     assert (
+#         board.__str__()
+#         == """
+#   a b c d e f g h i
+# 9 r h e a k a e h r  9
+# 8 . . . . . . . . .  8
+# 7 . c . . . . . c .  7
+# 6 p . p . p . p . p  6
+# 5 . . . . . . . . .  5
+# 4 . . . . . . . . .  4
+# 3 P . P . P . P . P  3
+# 2 . C . . . . . C .  2
+# 1 . . . . . . . . .  1
+# 0 R H E A K A E H R  0
+#   a b c d e f g h i
+# """
+#     )
+
+#     board.set_language(Language.ZH)
+#     assert (
+#         board.__str__()
+#         == """
+# 　ａｂｃｄｅｆｇｈｉ
+# ９車馬相仕帥仕相馬車９
+# ８．．．．．．．．．８
+# ７．砲．．．．．砲．７
+# ６卒．卒．卒．卒．卒６
+# ５．．．．．．．．．５
+# ４．．．．．．．．．４
+# ３兵．兵．兵．兵．兵３
+# ２．炮．．．．．炮．２
+# １．．．．．．．．．１
+# ０车马象士将士象马车０
+# 　ａｂｃｄｅｆｇｈｉ
+# """
+#     )
+
+#     assert board.king_locations == ["e0", "e9"]
+
+
+# def test_init_with_specified_fen():
+#     board = Board.from_fen("4kaR2/4a4/3hR4/7H1/9/9/9/9/4Ap1r1/3AK3c w---1 ")
+
+#     assert (
+#         board.__str__()
+#         == """
+#   a b c d e f g h i
+# 9 . . . A K . . . c  9
+# 8 . . . . A p . r .  8
+# 7 . . . . . . . . .  7
+# 6 . . . . . . . . .  6
+# 5 . . . . . . . . .  5
+# 4 . . . . . . . . .  4
+# 3 . . . . . . . H .  3
+# 2 . . . h R . . . .  2
+# 1 . . . . a . . . .  1
+# 0 . . . . k a R . .  0
+#   a b c d e f g h i
+# """
+#     )
+
+#     board.set_language(Language.ZH)
+#     assert (
+#         board.__str__()
+#         == """
+# 　ａｂｃｄｅｆｇｈｉ
+# ９．．．士将．．．砲９
+# ８．．．．士卒．車．８
+# ７．．．．．．．．．７
+# ６．．．．．．．．．６
+# ５．．．．．．．．．５
+# ４．．．．．．．．．４
+# ３．．．．．．．马．３
+# ２．．．馬车．．．．２
+# １．．．．仕．．．．１
+# ０．．．．帥仕车．．０
+# 　ａｂｃｄｅｆｇｈｉ
+# """
+#     )
+
+#     assert board.king_locations == ["e0", "e9"]
+
+
+def test_path_pieces():
+    board = Board.from_fen()
+    # In case of only moving one step, return empty path pieces.
+    assert board.path_pieces("a0", "a1") == ([], [])
+
+    # In case of advisor moving, return empty path pieces.
+    assert board.path_pieces("d0", "e1") == ([], [])
+
+    # In case of elephant move, there should be at most one path piece.
+    assert board.path_pieces("c0", "e2") == ([], [])
+    # Add one classical piece in the path.
+    board.board["d1"].reset(Piece("d1", SquareState.OCCUPIED, Type.ROOK, Color.RED))
+    assert board.path_pieces("c0", "e2") == (["d1"], [])
+    # Add one quantum piece in the path.
+    board.board["d1"].is_entangled = True
+    assert board.path_pieces("c0", "e2") == ([], ["d1"])
+
+    # Horizontal move
+    board.board["c7"].reset(Piece("c7", SquareState.OCCUPIED, Type.ROOK, Color.RED))
+    board.board["c7"].is_entangled = True
+    assert board.path_pieces("a7", "i7") == (["b7", "h7"], ["c7"])
+
+    # Vertical move
+    assert board.path_pieces("c0", "c9") == (["c3", "c6"], ["c7"])
+
+    # In case of horse move, there should be at most one path piece.
+    assert board.path_pieces("b9", "a7") == ([], [])
+    assert board.path_pieces("b9", "c7") == ([], [])
+    # One classical piece in path.
+    assert board.path_pieces("b9", "d8") == (["c9"], [])
+    # One quantum piece in path.
+    assert board.path_pieces("c8", "d6") == ([], ["c7"])
+
+
+def test_flying_general_check():
+    board = Board.from_fen()
+    # If they are in different columns, the check fails.
+    board.king_locations = ["d0", "e9"]
+    assert board.flying_general_check() == False
+
+    # If there are classical pieces between two KINGs, the check fails.
+    board.king_locations = ["e0", "e9"]
+    assert board.flying_general_check() == False
+
+    # If there are no pieces between two KINGs, the check successes.
+    board.board["e3"].reset()
+    board.board["e6"].reset()
+    assert board.flying_general_check() == True
+
+    # When there are quantum pieces in between.
+    board = set_board(["a3", "a4", "e0", "e9"])
+    board.king_locations = ["e0", "e9"]
+    board.current_player = 0  # i.e. RED
+    world = board.board
+    SplitJump()(world["a3"], world["c3"], world["e3"])
+    SplitJump()(world["a4"], world["c4"], world["e4"])
+
+    result = board.flying_general_check()
+    # We check the ancilla to learn whether the general/king flies or not.
+    captured = world.post_selection[world["ancilla_ancilla_flying_general_check_0_0"]]
+    if captured:
+        assert result
+        assert_samples_in(board, {locations_to_bitboard(["c3", "c4", "e0"]): 1.0})
+    else:
+        assert not result
+        assert_sample_distribution(
+            board,
+            {
+                locations_to_bitboard(["e0", "e9", "e3", "e4"]): 1.0 / 3,
+                locations_to_bitboard(["e0", "e9", "e3", "c4"]): 1.0 / 3,
+                locations_to_bitboard(["e0", "e9", "c3", "e4"]): 1.0 / 3,
+            },
+        )
---
+++ unitary/examples/quantum_chinese_chess/chess_test.py
@@ -0,0 +1,358 @@
+# Copyright 2023 The Unitary Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import pytest
+import io
+import sys
+from unitary.examples.quantum_chinese_chess.chess import QuantumChineseChess
+from unitary.examples.quantum_chinese_chess.piece import Piece
+from unitary.examples.quantum_chinese_chess.enums import (
+    Language,
+    Color,
+    Type,
+    SquareState,
+    MoveType,
+    MoveVariant,
+)
+
+
+def test_game_init(monkeypatch):
+    inputs = iter(["y", "Bob", "Ben"])
+    monkeypatch.setattr("builtins.input", lambda _: next(inputs))
+    output = io.StringIO()
+    sys.stdout = output
+    game = QuantumChineseChess()
+    assert game.lang == Language.ZH
+    assert game.players_name == ["Bob", "Ben"]
+    assert game.current_player == 0
+    assert "Welcome" in output.getvalue()
+    sys.stdout = sys.__stdout__
+
+
+def test_parse_input_string_success(monkeypatch):
+    inputs = iter(["y", "Bob", "Ben"])
+    monkeypatch.setattr("builtins.input", lambda _: next(inputs))
+    game = QuantumChineseChess()
+    assert game.parse_input_string("a1b1") == (["a1"], ["b1"])
+    assert game.parse_input_string("a1b1^c2") == (["a1", "b1"], ["c2"])
+    assert game.parse_input_string("a1^b1c2") == (["a1"], ["b1", "c2"])
+
+
+def test_parse_input_string_fail(monkeypatch):
+    inputs = iter(["y", "Bob", "Ben"])
+    monkeypatch.setattr("builtins.input", lambda _: next(inputs))
+    game = QuantumChineseChess()
+    with pytest.raises(ValueError, match="Invalid sources/targets string "):
+        game.parse_input_string("a1^b1")
+    with pytest.raises(ValueError, match="Invalid sources/targets string "):
+        game.parse_input_string("a^1b1c2")
+    with pytest.raises(ValueError, match="Two sources should not be the same."):
+        game.parse_input_string("a1a1^c2")
+    with pytest.raises(ValueError, match="Two targets should not be the same."):
+        game.parse_input_string("a1^c2c2")
+    with pytest.raises(ValueError, match="Invalid sources/targets string "):
+        game.parse_input_string("a1b")
+    with pytest.raises(ValueError, match="Source and target should not be the same."):
+        game.parse_input_string("a1a1")
+    with pytest.raises(ValueError, match="Invalid location string."):
+        game.parse_input_string("a1n1")
+
+
+def test_apply_move_fail(monkeypatch):
+    inputs = iter(["y", "Bob", "Ben"])
+    monkeypatch.setattr("builtins.input", lambda _: next(inputs))
+    game = QuantumChineseChess()
+    with pytest.raises(ValueError, match="Could not move empty piece."):
+        game.apply_move("a8b8")
+    with pytest.raises(ValueError, match="Could not move the other player's piece."):
+        game.apply_move("a9b8")
+    with pytest.raises(ValueError, match="Two sources need to be the same type."):
+        game.apply_move("a0a3^a4")
+    with pytest.raises(ValueError, match="Two targets need to be the same type."):
+        game.apply_move("b2^a2h2")
+    with pytest.raises(ValueError, match="Two targets need to be the same color."):
+        game.apply_move("b2^b7h2")
+
+
+def test_game_invalid_move(monkeypatch):
+    output = io.StringIO()
+    sys.stdout = output
+    inputs = iter(["y", "Bob", "Ben", "a1n1", "exit"])
+    monkeypatch.setattr("builtins.input", lambda _: next(inputs))
+    game = QuantumChineseChess()
+    game.play()
+    assert (
+        "Invalid location string. Make sure they are from a0 to i9."
+        in output.getvalue()
+    )
+    sys.stdout = sys.__stdout__
+
+
+def test_check_classical_rule(monkeypatch):
+    output = io.StringIO()
+    sys.stdout = output
+    inputs = iter(["y", "Bob", "Ben"])
+    monkeypatch.setattr("builtins.input", lambda _: next(inputs))
+    game = QuantumChineseChess()
+    # The move is blocked by classical path piece.
+    with pytest.raises(ValueError, match="The path is blocked."):
+        game.check_classical_rule("a0", "a4", ["a3"])
+
+    # Target should not be a classical piece of the same color.
+    with pytest.raises(
+        ValueError, match="The target place has classical piece with the same color."
+    ):
+        game.check_classical_rule("a0", "a3", [])
+
+    # ROOK
+    game.check_classical_rule("a0", "a2", [])
+    with pytest.raises(ValueError, match="ROOK cannot move like this."):
+        game.check_classical_rule("a0", "b1", [])
+
+    # HORSE
+    game.check_classical_rule("b0", "c2", [])
+    with pytest.raises(ValueError, match="HORSE cannot move like this."):
+        game.check_classical_rule("b0", "c1", [])
+
+    # ELEPHANT
+    game.check_classical_rule("c0", "e2", [])
+    with pytest.raises(ValueError, match="ELEPHANT cannot move like this."):
+        game.check_classical_rule("c0", "e1", [])
+    game.board.board["g5"].reset(
+        Piece("g5", SquareState.OCCUPIED, Type.ELEPHANT, Color.BLACK)
+    )
+    with pytest.raises(ValueError, match="ELEPHANT cannot cross the river"):
+        game.check_classical_rule("g5", "i3", [])
+    game.board.board["c4"].reset(
+        Piece("c4", SquareState.OCCUPIED, Type.ELEPHANT, Color.RED)
+    )
+    with pytest.raises(ValueError, match="ELEPHANT cannot cross the river"):
+        game.check_classical_rule("c4", "e6", [])
+
+    # ADVISOR
+    game.check_classical_rule("d9", "e8", [])
+    with pytest.raises(ValueError, match="ADVISOR cannot move like this."):
+        game.check_classical_rule("d9", "d8", [])
+    with pytest.raises(ValueError, match="ADVISOR cannot leave the palace."):
+        game.check_classical_rule("d9", "c8", [])
+    with pytest.raises(ValueError, match="ADVISOR cannot leave the palace."):
+        game.check_classical_rule("f0", "g1", [])
+
+    # KING
+    game.check_classical_rule("e9", "e8", [])
+    with pytest.raises(ValueError, match="KING cannot move like this."):
+        game.check_classical_rule("e9", "d8", [])
+    game.board.board["c0"].reset()
+    game.board.board["d0"].reset(game.board.board["e0"])
+    game.board.board["e0"].reset()
+    with pytest.raises(ValueError, match="KING cannot leave the palace."):
+        game.check_classical_rule("d0", "c0", [])
+
+    # CANNON
+    game.check_classical_rule("b7", "b4", [])
+    with pytest.raises(ValueError, match="CANNON cannot move like this."):
+        game.check_classical_rule("b7", "a8", [])
+    # Cannon could jump across exactly one piece.
+    game.check_classical_rule("b2", "b9", ["b7"])
+    with pytest.raises(ValueError, match="CANNON cannot fire like this."):
+        game.check_classical_rule("b2", "b9", ["b5", "b7"])
+    # Cannon cannot fire to a piece with same color.
+    game.board.board["b3"].reset(game.board.board["b2"])
+    game.board.board["b2"].reset()
+    game.board.board["e3"].is_entangled = True
+    with pytest.raises(
+        ValueError, match="CANNON cannot fire to a piece with same color."
+    ):
+        game.check_classical_rule("b3", "e3", ["c3"])
+    with pytest.raises(ValueError, match="CANNON cannot fire to an empty piece."):
+        game.check_classical_rule("b3", "d3", ["c3"])
+
+    # PAWN
+    game.check_classical_rule("a3", "a4", [])
+    with pytest.raises(ValueError, match="PAWN cannot move like this."):
+        game.check_classical_rule("a3", "a5", [])
+    with pytest.raises(
+        ValueError, match="PAWN can only go forward before crossing the river"
+    ):
+        game.check_classical_rule("e3", "f3", [])
+    with pytest.raises(
+        ValueError, match="PAWN can only go forward before crossing the river"
+    ):
+        game.check_classical_rule("g6", "h6", [])
+    with pytest.raises(ValueError, match="PAWN can not move backward."):
+        game.check_classical_rule("a3", "a2", [])
+    with pytest.raises(ValueError, match="PAWN can not move backward."):
+        game.check_classical_rule("g6", "g7", [])
+    # After crossing the rive the pawn could move horizontally.
+    game.board.board["c4"].reset(game.board.board["c6"])
+    game.board.board["c6"].reset()
+    game.check_classical_rule("c4", "b4", [])
+    game.check_classical_rule("c4", "d4", [])
+
+
+def test_classify_move_fail(monkeypatch):
+    output = io.StringIO()
+    sys.stdout = output
+    inputs = iter(["y", "Bob", "Ben"])
+    monkeypatch.setattr("builtins.input", lambda _: next(inputs))
+    game = QuantumChineseChess()
+    with pytest.raises(
+        ValueError, match="CANNON could not fire/capture without a cannon platform."
+    ):
+        game.classify_move(["b7"], ["b2"], [], [], [], [])
+
+    with pytest.raises(
+        ValueError, match="Both sources need to be in quantum state in order to merge."
+    ):
+        game.classify_move(["b2", "h2"], ["e2"], [], [], [], [])
+
+    game.board.board["c0"].reset(game.board.board["b7"])
+    game.board.board["c0"].is_entangled = True
+    game.board.board["b7"].reset()
+    game.board.board["g0"].reset(game.board.board["h7"])
+    game.board.board["g0"].is_entangled = True
+    game.board.board["h7"].reset()
+    with pytest.raises(ValueError, match="Currently CANNON cannot merge while firing."):
+        game.classify_move(["c0", "g0"], ["e0"], ["d0"], [], ["f0"], [])
+
+    game.board.board["b3"].reset(game.board.board["b2"])
+    game.board.board["b3"].is_entangled = True
+    game.board.board["b2"].reset()
+    game.board.board["d3"].reset(game.board.board["h2"])
+    game.board.board["d3"].is_entangled = True
+    game.board.board["h2"].reset()
+    with pytest.raises(
+        ValueError, match="Currently we could only merge into an empty piece."
+    ):
+        game.classify_move(["b3", "d3"], ["c3"], [], [], [], [])
+
+    with pytest.raises(ValueError, match="Currently CANNON cannot split while firing."):
+        game.classify_move(["g0"], ["e0", "i0"], ["f0"], [], ["h0"], [])
+
+    game.board.board["d0"].is_entangled = True
+    with pytest.raises(
+        ValueError, match="Currently we could only split into empty pieces."
+    ):
+        game.classify_move(["d3"], ["d0", "d4"], [], [], [], [])
+
+    game.board.board["d0"].reset()
+    game.board.board["f0"].reset()
+    with pytest.raises(ValueError, match="King split is not supported currently."):
+        game.classify_move(["e0"], ["d0", "f0"], [], [], [], [])
+
+
+def test_classify_move_success(monkeypatch):
+    output = io.StringIO()
+    sys.stdout = output
+    inputs = iter(["y", "Bob", "Ben"])
+    monkeypatch.setattr("builtins.input", lambda _: next(inputs))
+    game = QuantumChineseChess()
+    # classical
+    assert game.classify_move(["h9"], ["g7"], [], [], [], []) == (
+        MoveType.CLASSICAL,
+        MoveVariant.CLASSICAL,
+    )
+    assert game.classify_move(["b2"], ["b9"], ["b7"], [], [], []) == (
+        MoveType.CLASSICAL,
+        MoveVariant.CLASSICAL,
+    )
+
+    # jump basic
+    game.board.board["c9"].is_entangled = True
+    assert game.classify_move(["c9"], ["e7"], [], [], [], []) == (
+        MoveType.JUMP,
+        MoveVariant.BASIC,
+    )
+    game.board.board["b2"].is_entangled = True
+    assert game.classify_move(["b2"], ["e2"], [], [], [], []) == (
+        MoveType.JUMP,
+        MoveVariant.BASIC,
+    )
+
+    # jump excluded
+    game.board.board["a3"].is_entangled = True
+    assert game.classify_move(["a0"], ["a3"], [], [], [], []) == (
+        MoveType.JUMP,
+        MoveVariant.EXCLUDED,
+    )
+
+    # jump capture
+    game.board.board["g4"].reset(game.board.board["g6"])
+    game.board.board["g4"].is_entangled = True
+    game.board.board["g6"].reset()
+    assert game.classify_move(["g4"], ["g3"], [], [], [], []) == (
+        MoveType.JUMP,
+        MoveVariant.CAPTURE,
+    )
+
+    # slide basic
+    assert game.classify_move(["a0"], ["a4"], [], ["a3"], [], []) == (
+        MoveType.SLIDE,
+        MoveVariant.BASIC,
+    )
+
+    # slide excluded
+    game.board.board["i7"].reset(game.board.board["h7"])
+    game.board.board["i7"].is_entangled = True
+    game.board.board["h7"].reset()
+    game.board.board["i6"].is_entangled = True
+    assert game.classify_move(["i9"], ["i6"], [], ["i7"], [], []) == (
+        MoveType.SLIDE,
+        MoveVariant.EXCLUDED,
+    )
+
+    # slide capture
+    assert game.classify_move(["a0"], ["a6"], [], ["a3"], [], []) == (
+        MoveType.SLIDE,
+        MoveVariant.CAPTURE,
+    )
+
+    # split_jump basic
+    assert game.classify_move(["g4"], ["f4", "h4"], [], [], [], []) == (
+        MoveType.SPLIT_JUMP,
+        MoveVariant.BASIC,
+    )
+
+    # split_slide basic
+    game.board.board["d3"].reset(game.board.board["h2"])
+    game.board.board["h2"].reset()
+    game.board.board["c3"].is_entangled = True
+    game.board.board["e3"].is_entangled = True
+    assert game.classify_move(["d3"], ["b3", "f3"], [], ["c3"], [], ["e3"]) == (
+        MoveType.SPLIT_SLIDE,
+        MoveVariant.BASIC,
+    )
+
+    # merge_jump basic
+    game.board.board["b7"].is_entangled = True
+    assert game.classify_move(["b7", "i7"], ["e7"], [], [], [], []) == (
+        MoveType.MERGE_JUMP,
+        MoveVariant.BASIC,
+    )
+
+    # merge_slide basic
+    assert game.classify_move(["b7", "i7"], ["a7"], [], [], [], ["b7"]) == (
+        MoveType.MERGE_SLIDE,
+        MoveVariant.BASIC,
+    )
+
+    # cannon_fire capture
+    assert game.classify_move(["i7"], ["i3"], [], ["i6"], [], []) == (
+        MoveType.CANNON_FIRE,
+        MoveVariant.CAPTURE,
+    )
+    game.board.board["i6"].is_entangled = False
+    assert game.classify_move(["i7"], ["i3"], ["i6"], [], [], []) == (
+        MoveType.CANNON_FIRE,
+        MoveVariant.CAPTURE,
+    )
