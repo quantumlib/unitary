1d0817f321fb71975f1c90c13d1cf3547f2af132
Peter Solodov <solodov@gmail.com>
2024-05-22 16:53:59 -0400

    Added explanation as to how to run tic tac toe now

---
+++ examples/tic_tac_toe/README.md
@@ -0,0 +1,7 @@
+# How to use the game
+
+This is a command line game. After cloning the Unitary library, change the directory into `examples/`. You can use command line flags to run the game:
+  
+```
+python -m tic_tac_toe.tic_tac_toe
+```
--- examples/tic_tac_toe/__init__.py
+++ examples/tic_tac_toe/__init__.py
@@ -11,6 +11,3 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from .tic_tac_toe import TicTacToe, GameInterface
-from .tic_tac_split import TicTacSplit
-from .enums import TicTacSquare, TicTacResult, TicTacRules
