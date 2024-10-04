a158b613138e7a30f46056c7d2b97e1a23bb3a17
Pavol Juhas <juhas@google.com>
2024-10-04 00:01:10 -0700

    pylint - fix wrong-or-nonexistent-copyright-notice

--- unitary/quantum_chess/circuit_transformer.py
+++ unitary/quantum_chess/circuit_transformer.py
@@ -1,4 +1,4 @@
-# Copyright 2020 Google
+# Copyright 2020 The Unitary Authors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
--- unitary/quantum_chess/readout_tester_test.py
+++ unitary/quantum_chess/readout_tester_test.py
@@ -1,4 +1,4 @@
-# Copyright 2020 Google
+# Copyright 2020 The Unitary Authors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,8 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
-#
+
 import cirq
 import cirq_google as cg
 
